5e3f139415b41f03e203787e24a9c6a6a8c114bb
YuanDing <1472939313@qq.com>
2023-12-31 21:47:45 +0800

    master 第四次提交

--- src/main/java/com/weike/test/App.java
+++ src/main/java/com/weike/test/App.java
@@ -10,8 +10,7 @@ package com.weike.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("你好，世界！");
-        System.out.println("1111111");
-        System.out.println("22222");
+        System.out.println("push test");
+
     }
 }
