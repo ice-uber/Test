a4cda128b6483697336b9a0c921c99eabc82e878
YuanDing <1472939313@qq.com>
2023-12-31 21:14:56 +0800

    第一次使用IDEA提交

---
+++ src/main/java/com/weike/test/App.java
@@ -0,0 +1,14 @@
+package com.weike.test;
+
+/**
+ * @ClassName: App
+ * @Author: YuanDing
+ * @Date: 2023/12/31 21:10
+ * @Description:
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
