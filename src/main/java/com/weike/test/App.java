2cda16de345b7f1a383ab0d3b5f4e0235a1cdf6d
YuanDing <1472939313@qq.com>
2023-12-31 21:34:37 +0800

    master的第三次提交

--- src/main/java/com/weike/test/App.java
+++ src/main/java/com/weike/test/App.java
@@ -11,5 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("你好，世界！");
+        System.out.println("22222");
     }
 }
