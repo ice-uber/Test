63cf2df752fee1d060938acbd96811f782d26665
YuanDing <130045567+ice-uber@users.noreply.github.com>
2023-12-31 21:52:55 +0800

    Update App.java
    网页端提交

--- src/main/java/com/weike/test/App.java
+++ src/main/java/com/weike/test/App.java
@@ -11,6 +11,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("push test");
+         System.out.println("pull test");
 
     }
 }
