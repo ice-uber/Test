2f885220069116f92aa554545c44caef6dac1c95
YuanDing <1472939313@qq.com>
2023-12-31 21:27:54 +0800

    hot fix 分支的第一次提交

--- src/main/java/com/weike/test/App.java
+++ src/main/java/com/weike/test/App.java
@@ -10,5 +10,6 @@ package com.weike.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("你好，世界！");
     }
 }
